42cc45e9ab23ef9333e576daab7c79970efbfb37
SUJITH PRABHU <125428582+sujith-juego@users.noreply.github.com>
2023-02-16 17:37:29 +0530

    Update task12.js

--- task12.js
+++ task12.js
@@ -1,4 +1,3 @@
-
 /*
 Given an array of dates, Find the particular day on which the date falls (like Monday, Tuesday). Return the objects with key as date and day as value. 
 Eg –  
@@ -6,32 +5,22 @@ input - arr = [‘12/02/2023’, ‘11/02/2023’, ‘10/02/2023’] 
 */
 
 
-let arr = new Array();
-arr = ["12/02/2023", "11/02/2023", "10/02/2023"];
+function dateday(arr){
 
-const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+  arr.sort();
+  const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+// mm/dd//yyyy
 
+  const obj = {};
+  let day = new Array();
 
-const obj = {};
-let day = new Array();
+  arr.forEach((elem,i) => {
+    const d = new Date(arr[i]);
+    day[i] = weekday[d.getDay()];
+    obj[`${arr[i]}`] = day[i]
+  })
+  return obj;
+}
+const output = dateday(["12/02/2023" , "11/02/2023", "10/20/2023","09/02/2023", "08/02/2023", "7/02/2023","8/02/2023", "7/02/2023", "8/15/2023","6/02/2023", "5/02/2023",,"4/02/2023"]);
 
-weekday.forEach((elem,i) => {
-  const d = new Date(arr[i]);
-  day[i] = weekday[d.getDay()];
-   obj[`${arr[i]}`] = day[i];
-})
-
-console.log(obj);
-
-//j = .getDay();
-
-//for(i=0;i<arr.length;i++){
-  // const obj = {arr[i],day[j]};
-//}
-
-//console.log(obj);
-
-//const d = new Date();
-//let day = d.getDay();
-//console.log(day);
-//console.log(d);
+console.log(output);
